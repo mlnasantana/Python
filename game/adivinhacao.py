033b3facba23a5448ca61374ce4e2912e9a5ebd9
Milena Santana <milenasantanamtv@gmail.com>
2022-08-04 20:12:19 -0300

    facilitando leitura do codigo

--- game/adivinhacao.py
+++ game/adivinhacao.py
@@ -10,12 +10,16 @@ print("Você digitou", chute_str)
 
 chute = int(chute_str)
 
-if numero_secreto == chute:
+acertou = chute == numero_secreto
+maior   = chute > numero_secreto
+menor   = chute < numero_secreto
+
+if (acertou):
     print("Você acertou")
 else:
-    if(chute > numero_secreto):
+    if(maior):
         print("Voce errou! O seu chute foi maior do que o numero secreto.")
-    elif(chute < numero_secreto):
+    elif(menor):
          print("Voce errou! O seu chute foi menor do que o numero secreto.")
 
 
