d0e5d4899c1f460cc9071b273d840412bd50d975
Milena Santana <milenasantanamtv@gmail.com>
2022-08-04 20:39:04 -0300

    testando laco for

--- game/adivinhacao.py
+++ game/adivinhacao.py
@@ -1,30 +1,26 @@
 print("*********************************")
 print("Bem vindo no jogo de adivinhação!")
 print("*********************************")
-
+ 
 numero_secreto = 42
-total_de_tentativa = 3
-rodada = 1
+total_de_tentativas = 3
 
-while(rodada <= total_de_tentativa):
-    print("Tentativa {} de {}", format(rodada, total_de_tentativa))
-    chute_str = input("Digite o seu numero: ")
-    print("Você digitou", chute_str)
+for rodada in range(1, total_de_tentativas + 1):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute_str = input("Digite o seu número: ")
+    print("Você digitou: ", chute_str)
     chute = int(chute_str)
 
-    acertou = chute == numero_secreto
-    maior   = chute > numero_secreto
-    menor   = chute < numero_secreto
+    acertou = numero_secreto == chute
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
     if (acertou):
-        print("Você acertou")
+        print("Você acertou!")
     else:
-        if(maior):
-            print("Voce errou! O seu chute foi maior do que o numero secreto.")
-        elif(menor):
-            print("Voce errou! O seu chute foi menor do que o numero secreto.")
-    
-    rodada = rodada + 1
-
+        if (maior):
+            print("Você errou! O seu chute foi maior que o número secreto.")
+        elif (menor):
+            print("Você errou! O seu chute foi menor que o número secreto.")
 
 print("Fim do jogo")
